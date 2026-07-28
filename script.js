7cc9c49fce702296e3180bc755ed88bda40f7136
mahaveercarcare <info.mahaveercarcare@gmail.com>
2026-07-28 13:15:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+window.addEventListener("scroll",function(){
 
+const header=document.querySelector(".header");
+
+if(window.scrollY>50){
+
+header.style.background="rgba(0,0,0,.90)";
+
+}
+
+else{
+
+header.style.background="rgba(0,0,0,.55)";
+
+}
+
+});
